192f8a0325beee8ceaa269b41680072d66edfe91
Wxhhaha <53258102+Wxhhaha@users.noreply.github.com>
2026-07-24 01:27:59 +0800

    Add direct-download HTML artifact

--- app/src/main/java/com/wxh/shareiot/api/EmptyBean.java
+++
@@ -1,8 +0,0 @@
-package com.wxh.shareiot.api;
-
-/**
- * 创建时间:2022/1/26 10:50
- * 作者:wxh
- */
-public class EmptyBean {
-}
--- app/src/main/java/com/wxh/shareiot/interfaces/SnackbarInterface.java
+++
@@ -1,9 +0,0 @@
-package com.wxh.shareiot.interfaces;
-
-/**
- * 创建时间:2021/12/15 10:17
- * 作者:wxh
- */
-public interface SnackbarInterface {
-    void ok();
-}
